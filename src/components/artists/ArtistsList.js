a91c9029c6565ecfd5f079b5133bb8e518f74b13
Uriel Fiori <uriel.fiori@gmail.com>
2021-12-03 15:40:15 -0300

    added error modal

--- src/components/artists/ArtistsList.js
+++ src/components/artists/ArtistsList.js
@@ -3,21 +3,28 @@ import React from "react";
 import Card from "../UI/Card";
 import ArtistsListItem from "./ArtistsListItem";
 import classes from "./ArtistsList.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const ArtistsList = (props) => {
   return (
-    <Card className={classes.artists}>
-      {props.artists.map((artist) => (
-        <ArtistsListItem
-          image={artist.image}
-          name={artist.name}
-          uri={artist.uri}
-          followers={artist.followers}
-          genres={artist.genres}
-          popularity={artist.popularity}
-        />
-      ))}
-    </Card>
+    <div>
+      <ErrorModal
+        title={props.artists[0].name}
+        message={props.artists[0].image}
+      />
+      <Card className={classes.artists}>
+        {props.artists.map((artist) => (
+          <ArtistsListItem
+            image={artist.image}
+            name={artist.name}
+            uri={artist.uri}
+            followers={artist.followers}
+            genres={artist.genres}
+            popularity={artist.popularity}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
