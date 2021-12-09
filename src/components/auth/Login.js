859e63fa2d46b375f0d8d117a45eb75d75ebb973
Uriel Fiori <uriel.fiori@gmail.com>
2021-12-09 10:28:17 -0300

    added cleanup to Login comp

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const id = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      clearTimeout(id);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
