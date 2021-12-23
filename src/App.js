aa9a6bc791bedfa24b57801c2ff49fa596a47998
Uriel Fiori <uriel.fiori@gmail.com>
2021-12-23 17:05:38 -0300

    added mdb to project and some styling to login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import { SpotifyApiContext } from "react-spotify-api";
 import { SpotifyAuth, Scopes } from "react-spotify-auth";
+import { MDBContainer } from "mdbreact";
 
 import Card from "./components/UI/Card";
 import MainHeader from "./components/nav/MainHeader";
 import ArtistsList from "./components/artists/ArtistsList";
 
+import AuthContext from "./store/auth-context";
 import SearchContext from "./store/search-context";
 
 import "./App.css";
 import "react-spotify-auth/dist/index.css";
-import AuthContext from "./store/auth-context";
 
 function App() {
   const artists = [
@@ -53,24 +54,32 @@ function App() {
 
   return (
     <React.Fragment>
-      {authContext.token ? (
-        <SpotifyApiContext.Provider value={authContext.token}>
-          <MainHeader />
-          <main>
-            <Card className="search-label">{searchContext.searchedTerm}</Card>
-            <ArtistsList artists={artists} />
-          </main>
-          <p>You are authorized with token: {authContext.token}</p>
-        </SpotifyApiContext.Provider>
-      ) : (
-        // Display the login page
-        <SpotifyAuth
-          redirectUri="http://localhost:3000/callback"
-          clientID="03e028b307ca44d687a7445042a004ef"
-          scopes={[Scopes.userReadPrivate, "user-read-email"]} // either style will work
-          onAccessToken={(token) => authContext.onLogin(token)}
-        />
-      )}
+      <MDBContainer>
+        {authContext.token ? (
+          <SpotifyApiContext.Provider value={authContext.token}>
+            <MainHeader />
+            <main>
+              <Card className="search-label">{searchContext.searchedTerm}</Card>
+              <ArtistsList artists={artists} />
+            </main>
+            <p>You are authorized with token: {authContext.token}</p>
+          </SpotifyApiContext.Provider>
+        ) : (
+          <div className="login-page">
+            <h1>Welcome to Spotify Library Manager</h1>
+            <h2>Sign in to get started</h2>
+            {/* Display the login page */}
+            <div className="spotifyBtn">
+              <SpotifyAuth
+                redirectUri="http://localhost:3000/callback"
+                clientID="03e028b307ca44d687a7445042a004ef"
+                scopes={[Scopes.userReadPrivate, "user-read-email"]} // either style will work
+                onAccessToken={(token) => authContext.onLogin(token)}
+              />
+            </div>
+          </div>
+        )}
+      </MDBContainer>
     </React.Fragment>
   );
 }
